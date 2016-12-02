afe8190594d99a9205c02ce42eefe7919e250fb0
rtso <rtso489@gmail.com>
2016-12-02 08:34:50 -0600

    Fixed mismatched ordering of input queues

--- reading_data.py
+++ reading_data.py
@@ -12,7 +12,7 @@ IMAGE_HEIGHT = 256
 IMAGE_WIDTH = 256
 IMAGE_DEPTH = 160
 NUM_CHANNELS = 1
-BATCH_SIZE = 10
+BATCH_SIZE = 5
 
 
 def encode_label(label):
@@ -48,42 +48,41 @@ def get_data(filenames):
     return data
 
 
-# Load images and label data
-filenames, labels = read_label_file(labels_file)
-all_filenames = filenames[:n]
-all_images = get_data(all_filenames)
-all_labels = labels[:n]
+def input_pipeline(labels_file):
+    # Load images and label data
+    filenames, labels = read_label_file(labels_file)
+    all_filenames = filenames[:n]
+    # all_images = get_data(all_filenames)
+    all_labels = labels[:n]
 
-# Start building input pipeline
-all_images = ops.convert_to_tensor(all_filenames, dtype=tf.string)
-all_labels = ops.convert_to_tensor(all_labels, dtype=tf.float32)
+    # Start building input pipeline
+    all_images = ops.convert_to_tensor(all_filenames, dtype=tf.string)
+    all_labels = ops.convert_to_tensor(all_labels, dtype=tf.float32)
 
-# Partition the data
-partitions = [0] * len(all_filenames)
-partitions[:test_set_size] = [1] * test_set_size
-random.shuffle(partitions)
-train_images, test_images = tf.dynamic_partition(all_images, partitions, 2)
-train_labels, test_labels = tf.dynamic_partition(all_labels, partitions, 2)
+    # Partition the data
+    partitions = [0] * len(all_filenames)
+    partitions[:test_set_size] = [1] * test_set_size
+    random.shuffle(partitions)
+    train_images, test_images = tf.dynamic_partition(all_images, partitions, 2)
+    train_labels, test_labels = tf.dynamic_partition(all_labels, partitions, 2)
 
-# Build input queues and define how to load images
-train_input_queue = tf.train.slice_input_producer(
-    [train_images, train_labels], shuffle=False)
-test_input_queue = tf.train.slice_input_producer(
-    [test_images, test_labels], shuffle=False)
+    # Build input queues and define how to load images
+    train_input_queue = tf.train.slice_input_producer(
+        [train_images, train_labels], shuffle=False)
+    test_input_queue = tf.train.slice_input_producer(
+        [test_images, test_labels], shuffle=False)
 
-train_image = train_input_queue[0]
-train_label = train_input_queue[1]
+    return train_input_queue, test_input_queue
 
-test_image = test_input_queue[0]
-test_label = test_input_queue[1]
+train_input_queue, test_input_queue = input_pipeline(labels_file)
 
 # Group samples into batches
-train_image_batch, train_label_batch = tf.train.batch(
-    [train_image, train_label],
-    batch_size=BATCH_SIZE)
-test_image_batch, test_label_batch = tf.train.batch(
-    [test_image, test_label],
-    batch_size=BATCH_SIZE)
+train_batch = tf.train.batch(
+    train_input_queue, batch_size=BATCH_SIZE,
+    num_threads=1)
+test_batch = tf.train.batch(
+    test_input_queue, batch_size=BATCH_SIZE,
+    num_threads=1)
 
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
@@ -93,11 +92,11 @@ with tf.Session() as sess:
     # print("".join([chr(item) for item in file_content.eval()]))
     print('from the train set:')
     for i in range(n - test_set_size):
-        print(sess.run(train_label_batch))
+        print(sess.run(train_batch))
 
     print('from the test set:')
-    for i in range(10):
-        print(sess.run(test_label_batch))
+    for i in range(test_set_size):
+        print(sess.run(test_batch))
 
     coord.request_stop()
     coord.join(threads)
